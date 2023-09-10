8635764279750dc39f2684343de8d871496ecf05
Константин <103624597+kotsazonov@users.noreply.github.com>
2023-09-10 13:25:04 +0300

    Revert "Home2"

--- Home_work4/task36.py
+++
@@ -1,8 +0,0 @@
-def print_operation_table(operation, num_rows=6, num_columns=6):
-    for i in range(1, num_rows + 1):
-        for j in range(1, num_columns + 1):
-            print(operation(i, j), end="\t")  # Используем табуляцию для разделения столбцов
-        print()  # Переходим на новую строку после завершения обработки столбца
-
-# Пример использования
-print_operation_table(lambda x, y: x * y)
